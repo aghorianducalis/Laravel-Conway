466a2a7d9fef1826d9f2afe0c15db993fce64b8e
aghorianducalis <aghorianducalis@gmail.com>
2023-04-09 03:58:55 +0300

    .env.example updated for postgresql

--- resources/views/conway.blade.php
+++ resources/views/conway.blade.php
@@ -456,29 +456,6 @@
             <form method="POST" action="">
                 @csrf
 
-                {{--            <fieldset disabled>--}}
-                {{--                <legend>Disabled fieldset example</legend>--}}
-                {{--                <div class="mb-3">--}}
-                {{--                    <label for="disabledTextInput" class="form-label">Disabled input</label>--}}
-                {{--                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">--}}
-                {{--                </div>--}}
-                {{--                <div class="mb-3">--}}
-                {{--                    <label for="disabledSelect" class="form-label">Disabled select menu</label>--}}
-                {{--                    <select id="disabledSelect" class="form-select">--}}
-                {{--                        <option>Disabled select</option>--}}
-                {{--                    </select>--}}
-                {{--                </div>--}}
-                {{--                <div class="mb-3">--}}
-                {{--                    <div class="form-check">--}}
-                {{--                        <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled>--}}
-                {{--                        <label class="form-check-label" for="disabledFieldsetCheck">--}}
-                {{--                            Can't check this--}}
-                {{--                        </label>--}}
-                {{--                    </div>--}}
-                {{--                </div>--}}
-                {{--            </fieldset>--}}
-
-                {{--            <input class="form-control" id="disabledInput" type="text" placeholder="Disabled input here..." disabled>--}}
                 <div class="form-group">
                     <label for="maximum_y">Rows</label>
                     <input type="number" max="10" min="10" class="form-control" aria-label="Rows" id="maximum_y" value="10">
